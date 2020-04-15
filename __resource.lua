140e6452dee9c8f39ee01797e2468167d3103839
Redven-Biker <marween35620@gmail.com>
2020-04-15 19:04:56 +0200

    1.0

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'img/logo.png',
+    'img/partenaire.jpg',
+    'img/icons/discord.png',
+    'img/icons/gamemode.png',
+    'img/icons/user.png',
+    'img/icons/www.png',
+    'music/loading.ogg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'

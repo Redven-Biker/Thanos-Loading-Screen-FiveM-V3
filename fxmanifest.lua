7d58ab98e8be803948d68449678c7486e04d957d
Redven-Biker <marween35620@gmail.com>
2021-03-26 07:35:03 +0100

    1.1
    - Change to fxmanifest.lua
    - fix music
    - add pause music space bar

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'img/logo.png',
+    'img/partenaire.jpg',
+    'img/icons/discord.png',
+    'img/icons/gamemode.png',
+    'img/icons/user.png',
+    'img/icons/www.png',
+    'music/music.mp3'
+}
